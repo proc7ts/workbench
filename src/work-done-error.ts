2a907df76841162935cdcb6953ae51fc892b78a3
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-01-06 18:05:32 +0700

    Document the `WorkDoneError`

--- src/work-done-error.ts
+++ src/work-done-error.ts
@@ -1,7 +1,18 @@
 import type { Workload } from './workload';
 
+/**
+ * An error indicating the work is already done.
+ */
 export class WorkDoneError<TWork = unknown> extends TypeError {
 
+  /**
+   * Constructs a work done error.
+   *
+   * @param workload - A workload the work is done for.
+   * @param work - The work already done, if any.
+   * @param reason - The reason of work failure, or `undefined` if the work completed successfully.
+   * @param message - Custom error message.
+   */
   constructor(
       readonly workload: Workload<TWork>,
       readonly work: TWork | undefined,
